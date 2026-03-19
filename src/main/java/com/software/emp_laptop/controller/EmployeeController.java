8859ce1caa799274c0a9618c51fedb1502b44766
ganesh714 <evvganesh1@gmail.com>
2026-03-19 07:50:38 +0530

    updated controllers

--- src/main/java/com/software/emp_laptop/controller/EmployeeController.java
+++ src/main/java/com/software/emp_laptop/controller/EmployeeController.java
@@ -1,7 +1,11 @@
 package com.software.emp_laptop.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -25,10 +29,25 @@ public class EmployeeController {
 		return employeeService.addEmployee(employee);
 	}
 	
-	@PutMapping("/employee/{emp_id}/laptop")
-	public Employee updateEmployee_LapId(@PathVariable String emp_id,@RequestBody Laptop lap_id) {
-		return employeeService.updateEmployee_LapId(emp_id,lap_id);
+	@GetMapping("/employees")
+	public List<Employee> getAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
+	
+	 @GetMapping("/employee/{id}")
+	public Employee getEmployeeById(@PathVariable String id) {
+		return employeeService.getEmployeeById(id);
 	}
-	
-	
+
+	@PutMapping("/employee/{id}")
+	public Employee updateEmployee(@PathVariable String id,
+									@RequestBody Employee employee) {
+		return employeeService.updateEmployee(id, employee);
+	}
+
+	@DeleteMapping("/employee/{id}")
+	public String deleteEmployee(@PathVariable String id) {
+		employeeService.deleteEmployee(id);
+		return "Employee deleted successfully!";
+	}
 }
