8e2dac05e0a3db15ad1bac67f2f85493a1188a5f
ganesh714 <evvganesh1@gmail.com>
2026-03-19 07:51:03 +0530

    updated models

--- src/main/java/com/software/emp_laptop/models/Employee.java
+++ src/main/java/com/software/emp_laptop/models/Employee.java
@@ -1,8 +1,9 @@
 package com.software.emp_laptop.models;
 
+import java.time.LocalDate;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
 
 @Entity
 public class Employee {
@@ -11,53 +12,59 @@ public class Employee {
 	private String emp_id;
 	private String emp_name;
 	private int emp_salary;
-	@OneToOne
-	private Laptop lid;
+	private LocalDate emp_joing_date;
+	
 	
 	public Employee() {}
 
-	public Employee(String emp_id, String emp_name, int emp_salary, Laptop lid) {
+
+	public Employee(String emp_id, String emp_name, int emp_salary, LocalDate emp_joing_date) {
 		super();
 		this.emp_id = emp_id;
 		this.emp_name = emp_name;
 		this.emp_salary = emp_salary;
-		this.lid= lid;
+		this.emp_joing_date = emp_joing_date;
 	}
-
 
 
 	public String getEmp_id() {
 		return emp_id;
 	}
 
+
 	public void setEmp_id(String emp_id) {
 		this.emp_id = emp_id;
 	}
+
 
 	public String getEmp_name() {
 		return emp_name;
 	}
 
+
 	public void setEmp_name(String emp_name) {
 		this.emp_name = emp_name;
 	}
+
 
 	public int getEmp_salary() {
 		return emp_salary;
 	}
 
+
 	public void setEmp_salary(int emp_salary) {
 		this.emp_salary = emp_salary;
 	}
 
-	public Laptop getLid() {
-		return lid;
+
+	public LocalDate getEmp_joing_date() {
+		return emp_joing_date;
 	}
 
-	public void setLid(Laptop lid) {
-		this.lid= lid;
-	}
-	
-	
 
+	public void setEmp_joing_date(LocalDate emp_joing_date) {
+		this.emp_joing_date = emp_joing_date;
+	}
+
+	
 }
