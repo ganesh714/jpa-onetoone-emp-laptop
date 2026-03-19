d5e925167d6e5592b094c2453ba3ceccfe2d2378
ganesh714 <evvganesh1@gmail.com>
2026-03-19 09:06:01 +0530

    updated repo

--- src/main/java/com/software/emp_laptop/repository/LaptopRepository.java
+++ src/main/java/com/software/emp_laptop/repository/LaptopRepository.java
@@ -1,6 +1,8 @@
 package com.software.emp_laptop.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.software.emp_laptop.models.Laptop;
@@ -8,4 +10,7 @@ import com.software.emp_laptop.models.Laptop;
 @Repository
 public interface LaptopRepository extends JpaRepository<Laptop, String>{
 	
+	
+	@Query("SELECT l FROM Laptop l WHERE l.employee.id = :id")
+    public Laptop getLaptopByEmpId(@Param("id") String id);
 }
